4928c0d4aa63ec6870d6a55ac3b0c3e1fa97d7ed
shayanahmad1999 <shayanahmad235@gmail.com>
2025-08-22 16:41:24 +0500

    searchable class are define

--- app/Models/Movie.php
+++ app/Models/Movie.php
@@ -4,12 +4,25 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 class Movie extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
 
     protected $table = 'movies';
     protected $primaryKey = 'id';
     protected $fillable = ['title', 'description', 'author', 'release_year'];
+
+    public function toSearchableArray(): array
+    {
+        return array_merge($this->toArray(), [
+            'id' => (string) $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'author' => $this->author,
+            'release_year' => (string) $this->release_year,
+            'created_at' => $this->created_at->timestamp,
+        ]);
+    }
 }
