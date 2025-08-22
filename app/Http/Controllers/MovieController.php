b035eb479d51bc31e6ba7fa8ab0c067528db48c2
shayanahmad1999 <shayanahmad235@gmail.com>
2025-08-22 14:46:14 +0500

    Movie Controller are created and all logics are done for the movies

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Movie;
+use Illuminate\Http\Request;
+
+class MovieController extends Controller
+{
+    public function index()
+    {
+        $movies = Movie::latest()->paginate(9);
+        return view('movies.index', compact('movies'));
+    }
+
+    public function create()
+    {
+        return view('movies.create');
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required',
+            'author' => 'required|string|max:255',
+            'release_year' => 'required|integer',
+        ]);
+
+        Movie::create($request->all());
+        return redirect()->route('movies.index')->with('success', 'Movie added successfully!');
+    }
+
+    public function edit(Movie $movie)
+    {
+        return view('movies.edit', compact('movie'));
+    }
+
+    public function update(Request $request, Movie $movie)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required',
+            'author' => 'required|string|max:255',
+            'release_year' => 'required|integer',
+        ]);
+
+        $movie->update($request->all());
+        return redirect()->route('movies.index')->with('success', 'Movie updated successfully!');
+    }
+
+    public function destroy(Movie $movie)
+    {
+        $movie->delete();
+        return redirect()->route('movies.index')->with('success', 'Movie deleted successfully!');
+    }
+}
